aa54c1be2bee459b608d664eca51c66a8d7cf83e
Felipe Fernandes Diogo <felipe.diogo@ilegra.com>
2017-05-25 17:16:39 -0300

    first commit

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import Board from './components/Board';
+
+import './index.css';
+
+ReactDOM.render(
+  <Board />,
+  document.getElementById('root')
+);
