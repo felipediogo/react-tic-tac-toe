f7299eb121762d9f93381419e1bb7d6aecf37356
Felipe Fernandes Diogo <felipe.diogo@ilegra.com>
2017-05-31 15:11:11 -0300

    fixes board

--- src/components/Board.js
+++ src/components/Board.js
@@ -12,9 +12,12 @@ function Square(props) {
 
 class Board extends React.Component {
   renderSquare(i) {
-    return <Square
-      value={this.props.squares[i]}
-      onClick={(i) =>this.props.onClick(i)} />;
+    return (
+      <Square
+        value={this.props.squares[i]}
+        onClick={() => this.props.onClick(i)}
+      />
+    );
   }
 
   render() {
